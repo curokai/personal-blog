5573c832ec3aca658e6110a8841afe7380a4c540
curokai <51101590+curokai@users.noreply.github.com>
2019-10-01 10:43:05 -0700

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,5 +1,8 @@
 $("sidebarCollapse").on("click",function()){
+  
+                        
   $("nav").toggleCalss("active");
   $(".wrapper").toggleCalss("menuup");
   $(".mobile-header").toggleCalss("hide");
+   
 }
